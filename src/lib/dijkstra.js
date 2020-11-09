750f18fc04b39f386ca4dc63ab7cb106e4d7198c
Tobi Kehinde <tokehinde11@gmail.com>
2020-11-08 19:17:46 -0700

    fixed Tolu super "difficult" test

--- src/lib/dijkstra.js
+++ src/lib/dijkstra.js
@@ -4,9 +4,10 @@ import DijkstraError from "./dijkstraError";
 const SHIFT_INDEX = 1;
 
 const startDijkstra = (nodes, startIndex) => {
-  if (startIndex > nodes.length) return new DijkstraError();
-
-  return nodes[startIndex - SHIFT_INDEX];
+    if (startIndex > nodes.length || startIndex < SHIFT_INDEX) return new DijkstraError();
+   
+    return nodes[startIndex - SHIFT_INDEX];
 };
+
 
 export default startDijkstra;
